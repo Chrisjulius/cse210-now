0620d7bada36d631d86e447082a3ec5771f9bb62
unknown <chrisogarjulius@gmail.com>
2023-01-07 18:06:55 +0100

    Prep3 activity done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int GuessNumber;
+        do
+        {
+            Console.Write("What is your guess? ");
+            GuessNumber = int.Parse(Console.ReadLine());
+
+            if (GuessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (GuessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (GuessNumber != magicNumber);
+        
+        
+
+        
+
+
     }
 }
