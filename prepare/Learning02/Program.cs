8ad7243613fc26ed40a68c495a7f1286a6f8f073
chrisjulius <chrisogarjulius@gmail.com>
2023-01-16 20:03:36 +0100

    completed class/object activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2012;
+        job1._endYear = 2013;
+
+        Job job2 = new Job();
+        job2._jobTitle = "UI/UX Engineer";
+        job2._company = "Amazon";
+        job2._startYear = 2014;
+        job2._endYear = 2015;
+
+        Resume resume = new Resume();
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume._name = "Julius Ogar";
+
+        resume.Display();
+
     }
 }
