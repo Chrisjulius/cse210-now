01c137566661140a1c034128e3d8d6d1753abc19
Julius Ogar <41205804+Chrisjulius@users.noreply.github.com>
2023-02-09 04:01:50 +0100

    Reference class completed

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -3,8 +3,39 @@ using System.Collections.Generic;
 
 public class Reference
 {
-    public Reference()
+    private string _book;
+    private int _chapter;
+    private int _verseStart;
+    private int _verseEnd;
+
+    public Reference(string book, int chapter, int verseStart)
     {
-        
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart;
+    }
+
+    public Reference(string book, int chapter, int verseStart, int verseEnd)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verseStart = verseStart
+        _verseEnd = verseEnd;
+    }
+
+    public string ScriptureReference()
+    {
+        string ref;
+
+        if (_verseEnd == 0)
+        {
+            ref = $"{_book} {_chapter}:{_verseStart}";
+            return ref;
+        }
+        else
+        {
+            ref = $"{_book} {_chapter}:{_verseStart}-{verseEnd}";
+            return ref;    
+        }
     }
 }
