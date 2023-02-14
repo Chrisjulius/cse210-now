ba706c0891ab1855072a3fcde8d9a082fb265f76
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-14 16:41:05 +0100

    progress

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -6,7 +6,7 @@ public class Reference
     private string _book;
     private int _chapter;
     private int _verseStart;
-    private int _verseEnd;
+    private int _verseEnd = 0;
 
     public Reference(string book, int chapter, int verseStart)
     {
--- prove/Develop03/Word.cs
+++ prove/Develop03/Word.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 public class Word
 {
+    private string _verseLine;
     public Word()
     {
-        
+        _verseLine = "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways acknowledge him and he shall direct thy paths.";
     }
+
+    public List<string> Words()
+    {
+        List<string> result = new List<string>(_verseLine.Split("[a-zA-Z] [a-zA-Z]"));
+        return result;
+    } 
+
 }
