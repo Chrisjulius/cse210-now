57fc4efdfcf83c4a35b7894c1b4ac4061c16e807
Chrisjulius <chrisogarjulius@gmail.com>
2023-02-16 15:49:05 +0100

    Learning Activity 4 complete

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment newAssign = new Assignment("Julius Ogar", "Calculus I");
+        Console.WriteLine(newAssign.GetSummary());
+
+        Console.WriteLine("");
+        MathAssignment newMath = new MathAssignment("Julius Ogar", "Calculus I", "7.1", "2-10");
+        Console.WriteLine(newMath.GetSummary());
+        Console.WriteLine(newMath.GetHomeworkList());
+
+        WritingAssignment newWriting = new WritingAssignment("Julius Ogar", "European History", "The causes of World War II");
+        Console.WriteLine("");
+        Console.WriteLine(newWriting.GetSummary());
+        Console.WriteLine(newWriting.GetWritingInformation());
+
     }
 }
