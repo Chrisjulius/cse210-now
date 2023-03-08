b1725774b901b40b8809b2421f3006a5fc92e4cd
Chrisjulius <chrisogarjulius@gmail.com>
2023-03-08 18:03:39 +0100

    corrected the reflection activity name

--- prove/Develop04/ReflectionActivity.cs
+++ prove/Develop04/ReflectionActivity.cs
@@ -34,7 +34,7 @@ public class ReflectionActivity : Activity
 
     public void Reflection()
     {   
-        CommenceMsg("Breathing Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+        CommenceMsg("Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
 
         Console.WriteLine("Consider the following prompt:\n\n");
         Console.WriteLine(GetPrompt());
